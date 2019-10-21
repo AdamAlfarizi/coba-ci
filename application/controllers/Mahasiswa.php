324f8da7737e2e2c065803a2ec9729621eab2309
adam <adamalfal8@mail.unpas.ac.id>
2019-10-22 02:12:37 +0700

    membuat fungsi dimana fungsi tersebuat dapat mencari mengubah menghapus melihat detail data menambahkan data dan harus semuanya di isi apa bila salah satu tidak di isi akan gagal

--- application/controllers/Mahasiswa.php
+++ application/controllers/Mahasiswa.php
@@ -2,34 +2,72 @@
 
 class Mahasiswa extends CI_Controller
 {
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('Mahasiswa_model');
+		$this->load->library('form_validation');
+	}
 	public function index()
 	{
-		$this->load->model('Mahasiswa_model');
+		$data['judul'] = 'Daptar Mahasiswa';
 		$data['mahasiswa'] = $this->Mahasiswa_model->getAllMahasiswa();
+		if($this->input->post('keyword')){
+			$data['mahasiswa'] = $this->Mahasiswa_model->cariDataMahasiswa();
+		}
+		$this->load->view('templates/header', $data);
 		$this->load->view('mahasiswa/index', $data);
+		$this->load->view('templates/footer');
 	}
 
 	public function tambah()
 	{
-		$this->load->library('form_validation');
-		$this->load->model('Mahasiswa_model');
+		$data['judul'] = 'Form Tambah Data Mahasiswa';
 
 		$this->form_validation->set_rules('nama', 'Nama', 'required');
-
-		if( $this->form_validation->run()== false){
+		$this->form_validation->set_rules('nrp', 'Nrp', 'required|numeric');
+		$this->form_validation->set_rules('email', 'Email', 'required|valid_email');
+		if($this->form_validation->run() == false){
+			$this->load->view('templates/header', $data);
 			$this->load->view('mahasiswa/tambah');
+			$this->load->view('templates/footer');
 		}else{
-			$data = [
-				'nama' => $this->input->post('nama'),
-				'nrp' => $this->input->post('nrp'),
-				'email' => $this->input->post('email'),
-				'jurusan' => $this->input->post('jurusan')
-			];
-			$this->db->insert('mahasiswa', $data);
-			redirect('mahasiswa/index');
+			$this->Mahasiswa_model->tambahDataMahasiswa();
+			$this->session->set_flashdata('flash', 'Ditambahkan');
+			redirect('mahasiswa');
+		}
+
+	}
+	public function hapus($id)
+	{
+		$this->Mahasiswa_model->hapusDataMahasiswa($id);
+		$this->session->set_flashdata('flash', 'Dihapus');
+		redirect('mahasiswa');
+	}
+
+	public function detail($id)
+	{
+		$data['judul'] = 'Detail data Mahasiswa';
+		$data['mahasiswa'] = $this->Mahasiswa_model->getMahasiswaById($id);
+		$this->load->view('templates/header', $data);
+		$this->load->view('mahasiswa/detail', $data);
+		$this->load->view('templates/footer');
+	}
+
+	public function ubah($id)
+	{
+		$data['judul'] = 'From Ubah data Mahasiswa';
+		$data['mahasiswa'] = $this->Mahasiswa_model->getMahasiswaById($id);
+		$data['jurusan'] = ['Teknik Informatika', 'Teknik Mesin', 'Teknik Pangan','Teknik Planologo','Teknik Lingkungan'];
+		if($this->form_validation->run() == false){
+			$this->load->view('templates/header', $data);
+			$this->load->view('mahasiswa/ubah', $data);
+			$this->load->view('templates/footer');
+		}else{
+			$this->Mahasiswa_model->ubahDataMahasiswa();
+			$this->session->set_flashdata('flash', 'Diubah');
+			redirect('mahasiswa');
 		}
 	}
 
 }
-
- ?>
--- application/views/mahasiswa/tambah.php
+++ application/views/mahasiswa/tambah.php
@@ -1,54 +1,45 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <div class="container">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+      <div class="row mt-3">
+        <div class="col-6">
 
-    <title>Hello, world!</title>
-  </head>
-  <body>
-    <div class="container">
-      <div class="row">
-        <div class="col-5">
-          <h3>Form Tambah Data Mahasiswa</h3>
-          <form method="post" action="<?= base_url('mahasiswa/tambah'); ?>">
+          <div class="card">
+            <div class="card-header">
+            Form Tambah Data Mahasiswa
+          </div>
+          <div class="card-body">
+          <form action="" method="post">
             <div class="form-group">
               <label for="nama">Nama</label>
               <input type="text" class ="form-control" id="nama" name="nama">
-              <?= form_error('nama'); ?>
+              <small class="form-text text-danger"><?= form_error('nama'); ?></small>
             </div>
-
             <div class="form-group">
               <label for="nrp">Nrp</label>
               <input type="text" class ="form-control" id="nrp" name="nrp">
+              <small class="form-text text-danger"><?= form_error('nrp'); ?></small>
             </div>
-
             <div class="form-group">
               <label for="email">Email</label>
-              <input type="text" class="form-control" id="email" name="email">
+              <input type="text" class ="form-control" id="email" name="email">
+              <small class="form-text text-danger"><?= form_error('email'); ?></small>
             </div>
-
             <div class="form-group">
               <label for="jurusan">Jurusan</label>
-              <input type="text" class="form-control" id="jurusan" name="jurusan">
+              <select class ="form-control" id="jurusan" name="jurusan">
+                <option value="Teknik Informatika">Teknik Informatika</option>
+                <option value="Teknik Indrustri">Teknik Indrustri</option>
+                <option value="Teknik Mesin">Teknik Mesin</option>
+                <option value="Teknik Pangan">Teknik Pangan</option>
+                <option value="Teknik Planologi">Teknik planologi</option>
+                <option value="Teknik lingkungan">Teknik Lingkungan</option>
+              </select>
             </div>
-            <button type="submit" class="btn btn-primary">submit</button>
+            <button type="submit" name="tambah" class="btn btn-primary float-right">Tambah Data</button>
           </form>
         </div>
       </div>
     </div>
+    </div>
+  </div>
 
-
-
-
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  </body>
-</html>
